51288ab0f458f55e3331f1e216ceb05d4667abf1
Reynaldoabv <reynaldob.webdev@gmail.com>
2020-06-23 21:14:39 -0400

    Updates

---
+++ precache-manifest.18c8b44c4725df9850dd7e3024e6a6df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9427fb1194e4324e0b6fc1a4b1b58fb1",
+    "url": "/meat-code/index.html"
+  },
+  {
+    "revision": "ac0871c2c4471ff23fdb",
+    "url": "/meat-code/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e27014597bfbfcdd952e",
+    "url": "/meat-code/static/css/main.6d66ddd6.chunk.css"
+  },
+  {
+    "revision": "ac0871c2c4471ff23fdb",
+    "url": "/meat-code/static/js/2.507961d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meat-code/static/js/2.507961d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27014597bfbfcdd952e",
+    "url": "/meat-code/static/js/main.92ce2911.chunk.js"
+  },
+  {
+    "revision": "cf66b8d267e133e1e290",
+    "url": "/meat-code/static/js/runtime-main.f60a5c9d.js"
+  },
+  {
+    "revision": "aa6605b75c93ead4a24fc397d6bd3397",
+    "url": "/meat-code/static/media/arrow.aa6605b7.svg"
+  },
+  {
+    "revision": "137184dba54b51a694902553fdf2f3e2",
+    "url": "/meat-code/static/media/facebook-icon.137184db.svg"
+  },
+  {
+    "revision": "38c977ebbcd1c29b110df99bf6184f99",
+    "url": "/meat-code/static/media/hero-background.38c977eb.jpg"
+  },
+  {
+    "revision": "bf37f5f129581f29f44345841c0e740b",
+    "url": "/meat-code/static/media/hero-shape.bf37f5f1.png"
+  },
+  {
+    "revision": "42609a9901f0368a9ce44cdd3fe00651",
+    "url": "/meat-code/static/media/instagram-icon.42609a99.svg"
+  },
+  {
+    "revision": "d0836d9e559de5dfbea72fd53c856d4a",
+    "url": "/meat-code/static/media/logo.d0836d9e.svg"
+  },
+  {
+    "revision": "1329efff629e7a197cec727238538b80",
+    "url": "/meat-code/static/media/youtube-icon.1329efff.svg"
+  }
+]);
